4f8dfd69a7ee7040f6e16f97df96c712484109a1
hieronymous-bean <jeffrey@jeffreyallanbrown.com>
2020-11-11 15:32:13 -0500

    Initial Commit + Config

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# brainbase
-:alembic: Lightweight knowledgebase application for personal wikis and documentation. 
+<img src="./.github/brainbase.png" alt="Brainbase" width="100%" />
+
+[![Development Status](https://img.shields.io/static/v1?label=status&message=Active&nbsp;Development&color=purple&style=flat-square&logo=open-source-initiative&logoColor=ffffff)](#) [![Version](https://img.shields.io/github/v/release/hieronymous-bean/brainbase?include_prereleases&style=flat-square)](#) [![Issues](https://img.shields.io/github/issues-raw/hieronymous-bean/brainbase?style=flat-square)](#) [![License](https://img.shields.io/github/license/hieronymous-bean/brainbase?style=flat-square)](#)
+
+
+[![built with gulp](https://img.shields.io/badge/gulp-builds_this_project-eb4a4b.svg?logo=data%3Aimage%2Fpng%3Bbase64%2CiVBORw0KGgoAAAANSUhEUgAAAAYAAAAOCAMAAAA7QZ0XAAAABlBMVEUAAAD%2F%2F%2F%2Bl2Z%2FdAAAAAXRSTlMAQObYZgAAABdJREFUeAFjAAFGRjSSEQzwUgwQkjAFAAtaAD0Ls2nMAAAAAElFTkSuQmCC&style=flat-square)](http://gulpjs.com/)
+
+
+:alembic: Full component and module toolkit for starting HTML-based projects. 
+
+## Introduction
+TBD
+
+## Table of Contents
+- [System Requirements](#system-requirements)
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Usage](#usage)
+- [Features](#features)
+- [Updates](#updates)
+- [Contributing](#contributing)
+- [Credits](#credits)
+
+## System Requirements
+- Node.js ~14.15.0
+- NPM ~6.14.8
+
+Brainbase uses a core set of packages for its functionality:
+
+*Focus*                             |  *Tool(s)*
+------------------------------------|------------------------------------------------------------------------------------
+Engine                              | Node.js
+Primary Functions                   | Javascript
+Build and Deployment Automation     | Gulp
+Front-End                           | Bootstrap
+Styling                             | SASS
+
+## :inbox_tray: Installation
+
+To get started, run:
+
+```
+npm run setup
+```
+
+This will install all required modules, run Gulp task automation, and compile application components. 
+
+```bash
+├──.github
+├── dist
+├── docs
+|   ├── documentation.html
+├── src /
+|   ├── assets /
+|   |   ├── data
+|   |   ├── fonts
+|   |   ├── html
+|   |   ├── images
+|   |   ├── js
+|   |   ├── scss
+|   |   ├── vendors
+|   ├── index.html
+.gitignore
+ gulpfile.js
+ LICENSE
+ package.json
+ README.md
+```
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+// // // // // // // // // / //
+// Import Required Packages //
+// // // // // // // // // //
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-dart-sass');
+const del = require('del');
+const concat = require('gulp-concat');
+const merge = require('merge-stream');
+const cleanCSS = require('gulp-clean-css');
+const { series } = require('gulp');
+
+
+// // // // // // // //
+// Define Constants //
+// /// // // // // //
+
+/* Directories */
+const dirs = {
+  app: 'src/',
+  assets: 'assets/'
+};
+
+/* Paths */
+const paths = {
+  assets: dirs.app + dirs.assets,
+  scss: dirs.app + dirs.assets + 'scss/',
+  css: dirs.app + dirs.assets + 'css/',
+  js: dirs.app + dirs.assets + 'js/',
+  vendors: dirs.app + dirs.assets + 'vendors/',
+  fonts: dirs.app + dirs.assets + 'fonts/',
+  images: dirs.app + dirs.assets + 'images/'
+}
+
+// // // // // // // // // // / //
+// Intial Setup & Vendor Tasks //
+// // // // // // // // // // // 
+
+gulp.task('cleanBuild', () => {
+    return del([paths.css, paths.js, paths.vendors]);
+});
+gulp.task('cloneVendorSCSS', () => {
+    var style1 = gulp.src('./node_modules/bootstrap/scss/*.scss').pipe(gulp.dest('./src/assets/vendors/bootstrap/scss'));
+    var style2 = gulp.src('./node_modules/bootstrap/scss/mixins/*.scss').pipe(gulp.dest('./src/assets/vendors/bootstrap/scss/mixins'));
+    var style3 = gulp.src('./node_modules/bootstrap/scss/utilities/*.scss').pipe(gulp.dest('./src/assets/vendors/bootstrap/scss/utilities'));
+    var style4 = gulp.src('./node_modules/bootstrap/scss/vendor/*.scss').pipe(gulp.dest('./src/assets/vendors/bootstrap/scss/vendor'));
+    return merge(style1,style2,style3,style4);
+});
+gulp.task('cloneVendorJS', () => {
+    var script1 = gulp.src('./node_modules/bootstrap/js/src/*.js').pipe(gulp.dest('./src/assets/vendors/bootstrap/js'));
+    var script2 = gulp.src('./node_modules/bootstrap/dist/js/*.js').pipe(gulp.dest('./src/assets/vendors/bootstrap/js'));
+    var script3 = gulp.src('./node_modules/jquery/dist/jquery.min.js').pipe(gulp.dest('./src/assets/vendors/jquery'));
+    return merge(script1,script2,script3);
+});
+
+gulp.task('initialSetup', series('cleanBuild','cloneVendorSCSS','cloneVendorJS'));
+
+
+// // // // // //
+// Build Files //
+// // // // // //
+
+gulp.task('buildCoreCSS', () => {
+    return gulp.src(['./src/assets/vendors/bootstrap/scss/bootstrap.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('core.min.css'))
+    .pipe(gulp.dest('./src/assets/css'));
+});
+gulp.task('buildCoreJS', () => {
+    return gulp.src(['./src/assets/vendors/jquery/jquery.min.js','./src/assets/vendors/bootstrap/js/bootstrap.min.js'])
+    .pipe(concat('core.min.js'))
+    .pipe(gulp.dest('./src/assets/js'));
+});
+gulp.task('buildThemeCSS', () => {
+    return gulp.src(['./src/assets/scss/main.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./src/assets/css'));
+});
+
+gulp.task('buildCoreFiles', series('buildCoreCSS','buildCoreJS','buildThemeCSS'));
+
+
+
+// // // // // //
+// Full Build //
+// // // // // /
+
+gulp.task('mainBuild', series('initialSetup','buildCoreFiles'));
